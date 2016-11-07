96ae4e21c70c2d4f8b4b4387e10ff6f379d8ffed
Alexander-Andrade <veimar.94@gmail.com>
2016-11-07 22:04:23 +0300

    qwer

--- bcd2seven_test.srcs/sim_1/new/etalon_test.vhd
+++ bcd2seven_test.srcs/sim_1/new/etalon_test.vhd
@@ -51,20 +51,20 @@ architecture Behavioral of etalon_test is
     file fetalon : text;
     
     signal in_vec : std_logic_vector(6 downto 0) := (others=> '0');
-    signal out_vec1 : std_logic_vector(6 downto 0) := (others=> '0');
-    signal out_vec2 : std_logic_vector(6 downto 0) := (others=> '0');
+    signal out_vec_behav : std_logic_vector(6 downto 0) := (others=> '0');
+    signal out_vec_dflow : std_logic_vector(6 downto 0) := (others=> '0');
 begin
     
-     UUT1: bcd2seven_behav port map (notLT => in_vec(0), notRBI => in_vec(1), a => in_vec(2), b => in_vec(3), c => in_vec(4), d => in_vec(5), bi_rbo => in_vec(6),A_out => out_vec1(0), B_out => out_vec1(1), C_out => out_vec1(2), D_out => out_vec1(3), E_out => out_vec1(4), F_out => out_vec1(5), G_out => out_vec1(6));
-     UUT2: bcd2seven_dflow port map (notLT => in_vec(0), notRBI => in_vec(1), a => in_vec(2), b => in_vec(3), c => in_vec(4), d => in_vec(5), bi_rbo => in_vec(6),A_out => out_vec2(0), B_out => out_vec2(1), C_out => out_vec2(2), D_out => out_vec2(3), E_out => out_vec2(4), F_out => out_vec2(5), G_out => out_vec2(6));
+     UUT1: bcd2seven_behav port map (notLT => in_vec(0), notRBI => in_vec(1), a => in_vec(2), b => in_vec(3), c => in_vec(4), d => in_vec(5), bi_rbo => in_vec(6),A_out => out_vec_behav(0), B_out => out_vec_behav(1), C_out => out_vec_behav(2), D_out => out_vec_behav(3), E_out => out_vec_behav(4), F_out => out_vec_behav(5), G_out => out_vec_behav(6));
+     UUT2: bcd2seven_dflow port map (notLT => in_vec(0), notRBI => in_vec(1), a => in_vec(2), b => in_vec(3), c => in_vec(4), d => in_vec(5), bi_rbo => in_vec(6),A_out => out_vec_dflow(0), B_out => out_vec_dflow(1), C_out => out_vec_dflow(2), D_out => out_vec_dflow(3), E_out => out_vec_dflow(4), F_out => out_vec_dflow(5), G_out => out_vec_dflow(6));
        
 
     process
        variable in_vec_line : line;
        variable out_vec_line : line;
        
-       variable in_vec_var : std_logic_vector(in_vec'length-1 downto 0) := (others=> '0');
-       variable out_vec_var : std_logic_vector(in_vec'length-1 downto 0) := (others=> '0');
+       variable in_vec_etalon : std_logic_vector(in_vec'length-1 downto 0) := (others=> '0');
+       variable out_vec_etalon : std_logic_vector(in_vec'length-1 downto 0) := (others=> '0');
     begin
        file_open(fetalon, "../../../etalon.txt", read_mode);
        
@@ -72,15 +72,15 @@ begin
         readline(fetalon, in_vec_line);
         readline(fetalon, out_vec_line);
         -- Pass the variable to a signal
-        read(in_vec_line, in_vec_var);
-        read(out_vec_line, out_vec_var);
-        in_vec <= in_vec_var;
+        read(in_vec_line, in_vec_etalon);
+        read(out_vec_line, out_vec_etalon);
+        in_vec <= in_vec_etalon;
         wait for 5 ns;
-        assert out_vec1 = out_vec_var
-        report "bcd2seven_behav fails" severity failure;
+        assert out_vec_behav = out_vec_etalon
+        report "bcd2seven_behav fails" severity error;
         
-        assert out_vec2 = out_vec_var
-        report "bcd2seven_dflow fails" severity failure;
+        assert out_vec_dflow = out_vec_etalon
+        report "bcd2seven_dflow fails" severity error;
        end loop;
        
        file_close(fetalon);
